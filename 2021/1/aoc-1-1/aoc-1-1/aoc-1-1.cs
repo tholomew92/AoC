fe8979d3164d2ffc7d7f721c06efe97716bc9be3
sega9727 <sebastian.gabrielsson@gmail.com>
2021-11-30 10:57:20 +0100

    more day 1 prep

---
+++ 2021/1/aoc-1-1/aoc-1-1/aoc-1-1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace aoc_1_1
+{
+    class Program
+    {
+        static string workdir = Environment.CurrentDirectory;
+        static string inputPath = new DirectoryInfo(workdir).Parent.Parent.Parent.Parent.Parent.ToString();
+        static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
+        //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
+
+
+        void PrintConsole() {
+            System.Console.WriteLine("Hello World");
+        
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.PrintConsole();
+        }
+    }
+}
