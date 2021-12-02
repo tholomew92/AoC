d284ac4c130439470fd609f2c359720b39e25f7b
sega9727 <sebastian.gabrielsson@gmail.com>
2021-12-02 09:51:28 +0100

    Added day 2

--- 2021/1/aoc-1-new/aoc-1-new/aoc-1-new.cs
+++ 2021/1/aoc-1-new/aoc-1-new/aoc-1-new.cs
@@ -6,34 +6,34 @@ namespace aoc_1_new
         static string inputPath = new DirectoryInfo(workdir).Parent.Parent.Parent.Parent.Parent.ToString();
         static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
         //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
-        int increases;
 
 
         void Run()
         {
+            int increases;
             var watch = new System.Diagnostics.Stopwatch();
             watch.Start();
-            FindIncreases(1);
+            increases = FindIncreases(1);
             watch.Stop();
             Console.WriteLine($"The result for part one is: {increases}");
             Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
-            watch.Reset(); 
-            FindIncreases(3);
+            watch.Reset();
+            increases = FindIncreases(3);
             watch.Stop();
             Console.WriteLine($"The result for part one is: {increases}");
             Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
         }
 
-        void FindIncreases(int increments)
+        int FindIncreases(int increments)
         {
-            increases = 0;
+            int count = 0;
             for (int i = 0; i < input.Count - increments; i++)
             {
                 int d1 = Int32.Parse(input[i]);
                 int d2 = Int32.Parse(input[i + increments]);
-                if (d2 > d1) increases++;
+                if (d2 > d1) count++;
             }
-
+            return count;
         }
 
 
---
+++ 2021/2/aoc-2/aoc-2/aoc-2.cs
@@ -0,0 +1,77 @@
+﻿namespace aoc_1_new
+{
+    class Program
+    {
+        static string workdir = Environment.CurrentDirectory;
+        static string inputPath = new DirectoryInfo(workdir).Parent.Parent.Parent.Parent.Parent.ToString();
+        static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
+        //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
+
+
+        void Run()
+        {
+            int result;
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            result = FindPosition();
+            watch.Stop();
+            Console.WriteLine($"The result for part one is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
+            watch.Reset();
+            result = FindPositionPart2();
+            watch.Stop();
+            Console.WriteLine($"The result for part one is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
+        }
+
+        int FindPosition()
+        {
+            int horizontal, depth;
+            horizontal = depth = 0; 
+            foreach(var line in input)
+            {
+                var instructions = line.Split(' ');
+                _ = instructions[0] switch
+                {
+                    "forward" => horizontal += Int32.Parse(instructions[1]),
+                    "up" => depth -= Int32.Parse(instructions[1]),
+                    "down" => depth += Int32.Parse(instructions[1]),
+                    _ => throw new Exception()
+                };
+            }
+            return horizontal * depth;
+        }
+
+        int FindPositionPart2()
+        {
+            int horizontal, depth, aim;
+            horizontal = depth = aim = 0;
+            foreach (var line in input)
+            {
+                var instructions = line.Split(' ');
+                switch (instructions[0])
+                {
+                    case "forward":
+                        horizontal += Int32.Parse(instructions[1]);
+                        depth += aim * Int32.Parse(instructions[1]);
+                        break;
+                    case "up":
+                        aim -= Int32.Parse(instructions[1]);
+                        break;
+                    case "down":
+                        aim += Int32.Parse(instructions[1]);
+                        break;
+                    default: throw new Exception();
+                }
+            }
+            return horizontal * depth;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+    }
+}
