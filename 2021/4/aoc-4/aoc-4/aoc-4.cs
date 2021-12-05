ed9de572031326d9548331dd2b755806b1cd4236
Sebastian Gabrielsson <sebastian.gabrielsson@gmail.com>
2021-12-05 17:26:31 +0100

    Adding day 4 and 5

---
+++ 2021/4/aoc-4/aoc-4/aoc-4.cs
@@ -0,0 +1,170 @@
+﻿namespace aoc_4
+{
+    class Program
+    {
+        static string workdir = Environment.CurrentDirectory;
+        static string inputPath = new DirectoryInfo(workdir).Parent.Parent.Parent.Parent.Parent.ToString();
+        static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
+        //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
+
+        private List<int> numbers = input[0].Split(',').Where(x => !string.IsNullOrWhiteSpace(x)).Select(int.Parse).ToList();
+
+        public List<int[,]> boards = new List<int[,]>();
+
+        void ParseBoards() { 
+            int[,] board = new int[5,5];
+            int rowNumber = 0;
+
+            foreach (var line in input.Skip(1))
+            {
+                if (string.IsNullOrWhiteSpace(line)) continue;
+
+                var row = line.Split(' ').Where(x => !string.IsNullOrWhiteSpace(x)).Select(int.Parse).ToArray();
+
+                for (int i = 0; i < row.Length; i++) board[rowNumber, i] = row[i];
+
+                rowNumber++;
+
+                if (rowNumber == 5)
+                {
+                    boards.Add(board);
+
+                    board = new int[5, 5];
+                    rowNumber = 0;
+
+                    continue;
+                }
+
+            }
+        }
+
+        void Run()
+        {
+            int result;
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            ParseBoards();
+            result = PartOne();
+            watch.Stop();
+            Console.WriteLine($"The result for part one is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
+            watch.Reset();
+            ParseBoards();
+            result = PartTwo();
+            watch.Stop();
+            Console.WriteLine($"The result for part one is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
+        }
+
+        int PartOne() 
+        {
+
+            foreach (int number in numbers)
+            {
+                foreach (var board in boards)
+                {
+                    for (int row = 0; row < 5; row++)
+                    {
+                        for (int column = 0; column < 5; column++)
+                        {
+                            if (board[row, column] == number)
+                            {
+                                board[row, column] = -1;
+                                bool winner = CheckIfWinner(board,row,column,number);
+                                if (winner) {
+
+                                    return number * GetWinningSum(board);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            return 1;
+        }
+
+        int PartTwo()
+        {
+            List<int[,]> winners = new List<int[,]>();
+            foreach (int number in numbers)
+            {
+                foreach (var board in boards)
+                {
+                    for (int row = 0; row < 5; row++)
+                    {
+                        for (int column = 0; column < 5; column++)
+                        {
+                            if (board[row, column] == number)
+                            {
+                                board[row, column] = -1;
+                                bool winner = false;
+                                if(!winners.Contains(board)) winner = CheckIfWinner(board, row, column, number);
+                                if (winner)
+                                {
+                                    winners.Add(board);
+                                    if (winners.Count == boards.Count) return number * GetWinningSum(board);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            return 1;
+        }
+
+        static bool CheckIfWinner(int[,] board, int row, int column, int number)
+        {
+            int winCount = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == column) continue;
+
+                if (board[row, i] == -1) winCount++;
+                if (winCount == 4)
+                {
+                    return true;
+                }
+            }
+            
+            winCount = 0;
+            for (int i = 0; i < 5; i++) {
+                if (i == row) continue;
+
+                if (board[i, column] == -1) winCount++;
+                if (winCount == 4)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static int GetWinningSum(int[,] board)
+        {
+            int sum = 0;
+            for (int row = 0; row < 5; row++) 
+            {
+                for (int column = 0; column < 5; column++) 
+                {
+                    if(board[row,column] != -1)
+                        sum = sum + board[row,column];
+                }
+            }
+            return sum;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+    }
+
+
+}
+
---
+++ 2021/5/aoc-5/aoc-5/Program.cs
@@ -0,0 +1,173 @@
+﻿namespace aoc_4
+{
+    class Program
+    {
+        static string workdir = Environment.CurrentDirectory;
+        static string inputPath = new DirectoryInfo(workdir).Parent.Parent.Parent.Parent.Parent.ToString();
+        static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
+        //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
+
+
+        int PartOne(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            foreach (var line in input) { 
+                var splits = line.Split("->",StringSplitOptions.TrimEntries);
+                var point1 = splits[0].Split(',');
+                var point2 = splits[1].Split(',');
+                int x1,y1,x2,y2;
+                x1 = Int32.Parse(point1[0]);
+                y1 = Int32.Parse(point1[1]);
+                x2 = Int32.Parse(point2[0]);
+                y2 = Int32.Parse(point2[1]);
+
+                if (x1 == x2)
+                {
+                    if (y1 < y2) 
+                    {
+                        for (int i = y1; i <= y2; i++) 
+                        {
+                            matrix[x1, i]++;
+                        }
+                    }
+                    else 
+                    {
+                        for (int i = y2; i <= y1; i++)
+                        {
+                            matrix[x1, i]++;
+                        }
+                    }
+                }
+                else if (y1 == y2) 
+                {
+                    if (x1 < x2)
+                    {
+                        for (int i = x1; i <= x2; i++)
+                        {
+                            matrix[i, y1]++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = x2; i <= x1; i++)
+                        {
+                            matrix[i, y1]++;
+                        }
+                    }
+                }
+
+
+            }
+            int count = 0;
+            for (int x = 0; x < size; x++) 
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    if (matrix[y,x ]>1) count++;
+                }
+            }
+
+            return count;
+        }
+
+        int PartTwo(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            foreach (var line in input)
+            {
+                var splits = line.Split("->", StringSplitOptions.TrimEntries);
+                var point1 = splits[0].Split(',');
+                var point2 = splits[1].Split(',');
+                int x1, y1, x2, y2;
+                x1 = Int32.Parse(point1[0]);
+                y1 = Int32.Parse(point1[1]);
+                x2 = Int32.Parse(point2[0]);
+                y2 = Int32.Parse(point2[1]);
+                int xDiff, yDiff;
+                xDiff = x1 - x2;
+                yDiff = y1 - y2;
+
+                if (x1 == x2 && y1 != y2)
+                {
+                    if (y1 < y2)
+                    {
+                        for (int i = y1; i <= y2; i++)
+                        {
+                            matrix[x1, i]++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = y2; i <= y1; i++)
+                        {
+                            matrix[x1, i]++;
+                        }
+                    }
+                }
+                else if (y1 == y2 && x1 != x2)
+                {
+                    if (x1 < x2)
+                    {
+                        for (int i = x1; i <= x2; i++)
+                        {
+                            matrix[i, y1]++;
+                        }
+                    }
+                    else
+                    {
+                        for (int i = x2; i <= x1; i++)
+                        {
+                            matrix[i, y1]++;
+                        }
+                    }
+                }
+                else if(xDiff == yDiff || xDiff == -yDiff)
+                {
+                    for (int xy = 0; xy < Math.Abs(xDiff) + 1; xy++)
+                    {
+                        var xCoord = x1 + ((xDiff < 0) ? xy : xy * -1);
+                        var yCoord = y1 + ((yDiff < 0) ? xy : xy * -1);
+                        matrix[xCoord, yCoord]++;
+                    }
+
+                }
+            }
+            int count = 0;
+            for (int x = 0; x < size; x++)
+            {
+                for (int y = 0; y < size; y++)
+                {
+                    if (matrix[x, y] > 1) count++;
+                }
+            }
+
+            return count;
+        }
+
+
+        void Run()
+        {
+            int result;
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            result = PartOne(1000);
+            watch.Stop();
+            Console.WriteLine($"The result for part one is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
+            watch.Reset();
+            result = PartTwo(1000);
+            watch.Stop();
+            Console.WriteLine($"The result for part two is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds.ToString()} ms");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+
+    }
+}
