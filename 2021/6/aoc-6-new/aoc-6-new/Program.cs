a9528d561f46f26d177d38ba3ab4539ff98d130b
sega9727 <sebastian.gabrielsson@gmail.com>
2021-12-06 14:48:40 +0100

    updated day 6

---
+++ 2021/6/aoc-6-new/aoc-6-new/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace aoc_6_new
+{
+    class Program
+    {
+        static string workdir = Environment.CurrentDirectory;
+        static string inputPath = new DirectoryInfo(workdir).Parent.Parent.Parent.Parent.Parent.ToString();
+        static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
+        //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
+
+        void GetFish(int days)
+        {
+            List<long> fishyInput = input[0].Split(',').Select(x => long.Parse(x)).ToList();
+            List<long> allFishy = new List<long>();
+
+            for (int i = 0; i <= 8; i++) allFishy.Add(fishyInput.Count(x => x == i));
+            for (int i = 0; i < days; i++)
+            {
+                allFishy[7] += allFishy[0];
+                allFishy.Add(allFishy[0]);
+                allFishy.RemoveAt(0);
+            }
+
+            Console.WriteLine(allFishy.Sum());
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.GetFish(80);
+            p.GetFish(256);
+        }
+    }
+}
--- 2021/6/aoc-6/aoc-6/Program.cs
+++ 2021/6/aoc-6/aoc-6/Program.cs
@@ -7,25 +7,40 @@
         static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
         //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
 
-        int PartOne() 
+        long PartOne() 
         {
-            List<int> allFishy = (input[0].Split(',', StringSplitOptions.RemoveEmptyEntries).Select(x => Int32.Parse(x)).ToList());
-            for(int i = 0; i < 80; i++) 
+            List<long> fishyInput = input[0].Split(',').Select(x => long.Parse(x)).ToList();
+            long[] allFishy = new long[9];
+            long[] allFishyTemp = new long[9];
+
+            foreach (var fish in fishyInput)
             {
-                List<int> allFishyTemp = allFishy;
-                for(int j = 0; j < allFishy.Count; j++)
-                {
-                    if (allFishy[j] == 0)
-                    {
-                        allFishyTemp[j] = 6;
-                        allFishyTemp.Add(9);
-                    }
-                    else allFishyTemp[j]--;
-                }
-                allFishy = allFishyTemp;
+                allFishy[fish]++;
             }
 
-            return allFishy.Count;
+            for (int i = 0; i < 80; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j == 0)
+                    {
+                        allFishyTemp[6] = allFishy[0];
+                        allFishyTemp[8] = allFishy[0];
+                    }
+                    else
+                    {
+                        allFishyTemp[j - 1] += allFishy[j];
+                    }
+
+                    if (j == 8)
+                    {
+                        allFishy = allFishyTemp;
+                        allFishyTemp = new long[9];
+                    }
+                }
+            }
+
+            return allFishy.Sum();
         }
 
         long PartTwo()
@@ -48,7 +63,7 @@
                         allFishyTemp[6] = allFishy[0];
                         allFishyTemp[8] = allFishy[0];
                     }
-                    else
+                    else 
                     {
                         allFishyTemp[j-1] += allFishy[j];
                     }
