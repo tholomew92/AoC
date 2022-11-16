aaf608aeb4a0242e774850c32709a09b17f7a055
tholomew92 <sebastian.gabrielsson@gmail.com>
2022-11-16 13:50:15 +0100

    Starting 2015 Day 8

---
+++ 2015/8/aoc-8-1/aoc-8-1/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+namespace aoc_10_new
+{
+    class Program
+    {
+    static string workdir = Environment.CurrentDirectory;
+    static string inputPath = new DirectoryInfo(workdir).Parent.Parent.Parent.Parent.Parent.ToString();
+    static readonly List<string> input = File.ReadAllLines(inputPath + "\\input.txt").ToList();
+        //static readonly List<string> input = File.ReadAllLines(inputPath + "\\test.txt").ToList();
+
+
+        long PartOne()
+        {
+            return 0;
+        }
+
+        long PartTwo()
+        {
+            return 0;
+        }
+
+        void Run()
+        {
+            long result;
+            var watch = new System.Diagnostics.Stopwatch();
+            watch.Start();
+            result = PartOne();
+            watch.Stop();
+            Console.WriteLine($"The result for part one is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds} ms");
+            watch.Reset();
+            watch.Start();
+            result = PartTwo();
+            watch.Stop();
+            Console.WriteLine($"The result for part one is: {result}");
+            Console.WriteLine($"Time is {watch.ElapsedMilliseconds} ms");
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Run();
+        }
+    }
+}
