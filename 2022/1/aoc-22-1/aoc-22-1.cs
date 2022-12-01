e0de42432f9e69a6431f142704665a6e0f89d794
tholomew92 <sebastian.gabrielsson@gmail.com>
2022-12-01 10:29:06 +0100

    Started 2022 day 1

---
+++ 2022/1/aoc-22-1/aoc-22-1.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.InteropServices;
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+var workDir = AppDomain.CurrentDomain.BaseDirectory;
+var path = new DirectoryInfo(workDir).Parent.Parent.Parent.Parent.ToString();
+var input = File.ReadAllLines(path + "\\input.txt").ToList();
+//var input = File.ReadAllLines(path + "\\test.txt").ToList();
+
+var calories = new List<int>();
+var cal = 0;
+for(int i = 0; i < input.Count; i++)
+{
+    var line = input[i];
+    if (line.Equals(""))
+    {
+        calories.Add(cal);
+        cal = 0;
+        continue;
+    }
+    cal += int.Parse(line);
+    if(i == (input.Count - 1)) calories.Add(cal);
+}
+
+var partOne = calories.OrderByDescending(x => x).First();
+Console.WriteLine($"The result for part one is: {partOne}");
+var partTwo = calories.OrderByDescending(x => x).Take(3).Sum();
+watch.Stop();
+Console.WriteLine($"The result for part one is: {partTwo}");
+Console.WriteLine($"Time is {watch.ElapsedMilliseconds} ms");
