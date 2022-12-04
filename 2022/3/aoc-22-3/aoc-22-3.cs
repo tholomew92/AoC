f4dcd2f0fc45db3dca521c8d846385cc563a676d
Sebastian Gabrielsson <sebastian.gabrielsson@gmail.com>
2022-12-04 17:58:22 +0100

    Day 3 and 4

---
+++ 2022/3/aoc-22-3/aoc-22-3.cs
@@ -0,0 +1,32 @@
+﻿var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+var workDir = AppDomain.CurrentDomain.BaseDirectory;
+var path = new DirectoryInfo(workDir).Parent.Parent.Parent.Parent.ToString();
+var input = File.ReadAllLines(path + "\\input.txt").ToList();
+//var input = File.ReadAllLines(path + "\\test.txt").ToList();
+
+var letterList = "-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+var partOne = 0;
+var partTwo = 0;
+for (int i = 0; i < input.Count; i++)
+{
+    var line = input[i];
+    var mid = line.Length / 2;
+    var first = line.Substring(0, mid);
+    var second = line.Substring(mid);
+
+    var common = first.Intersect(second).Single();
+
+    partOne += Array.IndexOf(letterList, common);
+    if(i%3 == 0)
+    {
+        var intersect = line.Intersect(input[i + 1].Intersect(input[i + 2])).Single();
+        partTwo += Array.IndexOf(letterList, intersect);
+    }
+}
+
+
+Console.WriteLine($"The result for part one is: {partOne}");
+Console.WriteLine($"The result for part one is: {partTwo}");
+watch.Stop();
+Console.WriteLine($"Time is {watch.ElapsedMilliseconds} ms");
