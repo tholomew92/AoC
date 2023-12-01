55e86394b7ad1dfe9980878fd2cbb809ccb9573f
Sebastian Gabrielsson <sebastian.gabrielsson@gmail.com>
2023-12-01 09:12:35 +0100

    Started 2023

---
+++ 2023/1/Mall1/aoc-23-1.cs
@@ -0,0 +1,90 @@
+﻿using System.Runtime.CompilerServices;
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+var workDir = AppDomain.CurrentDomain.BaseDirectory;
+var path = new DirectoryInfo(workDir).Parent.Parent.Parent.ToString();
+var inputData = File.ReadAllLines(path + "\\input.txt").ToList();
+var testData = File.ReadAllLines(path + "\\test.txt").ToList();
+
+var input = inputData;
+var partOne = new List<int>();
+var resultOne = 0;
+var partTwo = new List<int>(); ;
+var resultTwo = 0;
+
+var parse = watch.Elapsed;
+/*foreach (var line in input)
+{
+    var listOne = new List<int>();
+    int value;
+    foreach (var c in line){
+        if (int.TryParse(c.ToString(), out value))
+        {
+            listOne.Add(value);
+        }
+    }
+    var newValue = int.Parse(listOne[0].ToString() + listOne.Last().ToString());
+    partOne.Add(newValue);
+}
+resultOne = partOne.Sum(x => x);*/
+var timeOne = watch.Elapsed - parse;
+
+
+string[] numbers = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+foreach (var line in input)
+{
+    var listTwo = new List<int>();
+    int value;
+    for ( int i = 0; i < line.Length; i++)
+    {
+        var c = line[i];
+        if (int.TryParse(c.ToString(), out value))
+        {
+            listTwo.Add(value);
+        }
+        for (int j = 0; j < numbers.Length; j++)
+        {
+            var num = numbers[j];
+            if (line.Length - i >= num.Length && line.Substring(i, num.Length).Equals(num))
+            {
+                listTwo.Add(j);
+            }
+
+        }
+    }
+    var newValue = int.Parse(listTwo[0].ToString() + listTwo.Last().ToString());
+    partTwo.Add(newValue);
+}
+resultTwo = partTwo.Sum(x => x);
+var timeTwo = watch.Elapsed - timeOne - parse;
+watch.Stop();
+Console.WriteLine($"The time for parsing input is: {FormattedTime(parse)}");
+Console.WriteLine($"The result for part one is: {resultOne}");
+Console.WriteLine($"The time for part one is {FormattedTime(timeOne)}");
+Console.WriteLine($"The result for part two is: {resultTwo}");
+Console.WriteLine($"The time for part two is {FormattedTime(timeTwo)}");
+Console.WriteLine($"Total time is {FormattedTime(watch.Elapsed)}");
+
+string FormattedTime(TimeSpan ts)
+{
+    var temp = ts.ToString("fffffff");
+    var count = 0;
+    foreach (var c in temp)
+    {
+        if (c.Equals('0')) count++;
+        else break;
+    }
+    var formatted = temp.Substring(count);
+    if (formatted.Length > 4)
+    {
+        formatted = formatted.Insert(formatted.Length - 4, ",");
+        formatted += " ms";
+    }
+    else
+    {
+        formatted += " µs";
+    }
+
+    return formatted;
+}
