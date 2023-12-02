039e32e6dd7f2d016c23126defb07af9baddb699
Sebastian Gabrielsson <sebastian.gabrielsson@gmail.com>
2023-12-02 09:53:34 +0100

    Did day 2

---
+++ 2023/2/aoc-23-2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System.Runtime.CompilerServices;
+
+var watch = new System.Diagnostics.Stopwatch();
+watch.Start();
+var workDir = AppDomain.CurrentDomain.BaseDirectory;
+var path = new DirectoryInfo(workDir).Parent.Parent.Parent.ToString();
+var inputDat = File.ReadAllLines(path + "\\input.txt").ToList();
+var testData = File.ReadAllLines(path + "\\test.txt").ToList();
+
+var input = inputDat;
+
+var partOne = 0;
+var partTwo = 0;
+
+var parse = watch.Elapsed;
+var maxRed = 12;
+var maxGreen = 13;
+var maxBlue = 14;
+
+foreach(var line in input)
+{
+    var index  = line.IndexOf(':')+1;
+    var data = line.Substring(index);
+    var split = data.Split(';', StringSplitOptions.TrimEntries);
+    var valid = true;
+    foreach(var item in split)
+    {
+        var cubes = item.Split(',', StringSplitOptions.TrimEntries);
+        foreach(var c in cubes)
+        {
+            var cube = c.Split(' ');
+            switch (cube[1])
+            {
+                case "red":
+                    if (int.Parse(cube[0]) > maxRed) valid = false;
+                    break;
+                case "green":
+                    if (int.Parse(cube[0]) > maxGreen) valid = false;
+                    break;
+                case "blue":
+                    if (int.Parse(cube[0]) > maxBlue) valid = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (!valid) break;
+    }
+    if (valid)
+    {
+        var gameNr = line.Substring(0, index - 1).Split(' ');
+        partOne += int.Parse(gameNr[1]);
+    }
+}
+
+var timeOne = watch.Elapsed - parse;
+
+
+foreach (var line in input)
+{
+    var minRed = 0;
+    var minGreen = 0;
+    var minBlue = 0;
+    var index = line.IndexOf(':') + 1;
+    var data = line.Substring(index);
+    var split = data.Split(';', StringSplitOptions.TrimEntries);
+    foreach (var item in split)
+    {
+        var cubes = item.Split(',', StringSplitOptions.TrimEntries);
+        foreach (var c in cubes)
+        {
+            var cube = c.Split(' ');
+            switch (cube[1])
+            {
+                case "red":
+                    if (int.Parse(cube[0]) > minRed) minRed = int.Parse(cube[0]);
+                    break;
+                case "green":
+                    if (int.Parse(cube[0]) > minGreen) minGreen = int.Parse(cube[0]);
+                    break;
+                case "blue":
+                    if (int.Parse(cube[0]) > minBlue) minBlue = int.Parse(cube[0]);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+    partTwo += (minRed*minGreen*minBlue);
+}
+var timeTwo = watch.Elapsed - timeOne - parse;
+watch.Stop();
+Console.WriteLine($"The time for parsing input is: {FormattedTime(parse)}");
+Console.WriteLine($"The result for part one is: {partOne}");
+Console.WriteLine($"The time for part one is {FormattedTime(timeOne)}");
+Console.WriteLine($"The result for part two is: {partTwo}");
+Console.WriteLine($"The time for part two is {FormattedTime(timeTwo)}");
+Console.WriteLine($"Total time is {FormattedTime(watch.Elapsed)}");
+
+string FormattedTime(TimeSpan ts)
+{
+    var temp = ts.ToString("fffffff");
+    var count = 0;
+    foreach (var c in temp)
+    {
+        if (c.Equals('0')) count++;
+        else break;
+    }
+    var formatted = temp.Substring(count);
+    if (formatted.Length > 4)
+    {
+        formatted = formatted.Insert(formatted.Length - 4, ",");
+        formatted += " ms";
+    }
+    else
+    {
+        formatted += " µs";
+    }
+
+    return formatted;
+}
