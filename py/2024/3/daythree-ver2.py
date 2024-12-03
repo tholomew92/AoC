c27f49356a825e69a32a5ebc09ba1f6555ce8bf8
tholomew92 <sebastian.gabrielsson@gmail.com>
2024-12-03 10:35:00 +0100

    cleaned up again

--- py/2024/3/daythree-ver2.py
+++ py/2024/3/daythree-ver2.py
@@ -7,9 +7,6 @@ testfile = open(testfilepath, "r")
 inputfilepath = os.path.join(here, "input.txt")
 inputfile = open(inputfilepath, "r")
 
-partone = 0
-parttwo = 0
-
 data = inputfile.read()
 findpattern = r"mul\((-?\d+),\s*(-?\d+)\)"
 removepattern = r"don't\(\).*?do\(\)"
@@ -19,7 +16,6 @@ matchestwo = [(int(x), int(y)) for x, y in re.findall(findpattern, removed)]
 
 partone = sum(a * b for a,b in matchesone)
 parttwo = sum(a * b for a,b in matchestwo)
-
 
 print(f"Part One: {partone}")
 print(f"Part Two: {parttwo}")
