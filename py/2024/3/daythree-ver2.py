1355006b722ffb36423384a29b04e2ba96b6527a
tholomew92 <sebastian.gabrielsson@gmail.com>
2024-12-03 10:33:07 +0100

    cleaned up again

--- py/2024/3/daythree-ver2.py
+++ py/2024/3/daythree-ver2.py
@@ -1,26 +1,25 @@
 import os
 import re
+import ast
 here = os.path.dirname(os.path.abspath(__file__))
 testfilepath = os.path.join(here, "test.txt")
 testfile = open(testfilepath, "r")
 inputfilepath = os.path.join(here, "input.txt")
 inputfile = open(inputfilepath, "r")
 
-def get_matches(matches):
-    res = 0
-    for match in matches:
-        res = int(match[0]) * int(match[1]) + res
-    return res 
+partone = 0
+parttwo = 0
 
 data = inputfile.read()
 findpattern = r"mul\((-?\d+),\s*(-?\d+)\)"
 removepattern = r"don't\(\).*?do\(\)"
-matchesone = re.findall(findpattern, data)
+matchesone = [(int(x), int(y)) for x, y in re.findall(findpattern, data)]
 removed = re.sub(removepattern, "", data, flags=re.DOTALL)
-matchestwo = re.findall(findpattern, removed)
+matchestwo = [(int(x), int(y)) for x, y in re.findall(findpattern, removed)]
 
-partone = get_matches(matchesone)
-parttwo = get_matches(matchestwo)
+partone = sum(a * b for a,b in matchesone)
+parttwo = sum(a * b for a,b in matchestwo)
+
 
 print(f"Part One: {partone}")
 print(f"Part Two: {parttwo}")
