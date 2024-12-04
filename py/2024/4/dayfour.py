87db6127b527bd1a4daeb2c7e42fff8ad0e73bb1
tholomew92 <sebastian.gabrielsson@gmail.com>
2024-12-04 10:37:44 +0100

    Cleaned up

--- py/2024/4/dayfour.py
+++ py/2024/4/dayfour.py
@@ -7,50 +7,56 @@ inputfile = open(inputfilepath, "r")
 
 data = inputfile.readlines()
 
+def downleft(x,y,data,steps):
+    sublist = []
+    for a in range(0,steps):
+        sublist.append(data[x+a][y-a])
+    return "".join(sublist)
+
+def downright(x,y,data,steps):
+    sublist = []
+    for a in range(0,steps):
+        sublist.append(data[x+a][y+a])
+    return "".join(sublist)
+
+def down(x,y,data,steps):
+    sublist = []
+    for a in range(0,steps):
+        sublist.append(data[x+a][y])
+    return "".join(sublist)
+
 partone = 0
-hits = ["XMAS", "SAMX"]
+parttwo = 0
+hitsone = ["XMAS", "SAMX"]
+hitstwo = ["MAS", "SAM"]
 for x in range(0, len(data)):
     for y in range(0, len(data[x].strip())):
-        hit = False
+
+        # Part One
         if y < len(data[x].strip()) - 3:
             substr = data[x][y:y+4]
-            if substr in hits:
-                hit = True
-                #print(f"x: {x}, y :{y} - Right")
+            if substr in hitsone:
                 partone = partone + 1
         if y > 2 and x < len(data) - 3:
-            dleft = []
-            dleft.append(data[x][y])
-            dleft.append(data[x+1][y-1])
-            dleft.append(data[x+2][y-2])
-            dleft.append(data[x+3][y-3])
-            substr = "".join(dleft)
-            if substr in hits:
-                hit = True
-                #print(f"x: {x}, y :{y} - Down Left")
+            downleftone = downleft(x,y,data,4)
+            if downleftone in hitsone:
                 partone = partone + 1
         if x < len(data) - 3:
-            down = []
-            down.append(data[x][y])
-            down.append(data[x+1][y])
-            down.append(data[x+2][y])
-            down.append(data[x+3][y])
-            substr = "".join(down)
-            if substr in hits:
-                hit = True
-                #print(f"x: {x}, y :{y} - Down")
+            downone = down(x,y,data,4)
+            if downone in hitsone:
                 partone = partone + 1
         if y < len(data[x].strip()) - 3 and x < len(data) - 3:
-            dright = []
-            dright.append(data[x][y])
-            dright.append(data[x+1][y+1])
-            dright.append(data[x+2][y+2])
-            dright.append(data[x+3][y+3])
-            substr = "".join(dright)
-            if substr in hits:
-                hit = True
-                #print(f"x: {x}, y :{y} - Down Right")
+            downrightone = downright(x,y,data,4)
+            if downrightone in hitsone:
                 partone = partone + 1
-    
+        
+        # Part Two
+        if y < len(data[x].strip()) - 2 and x < len(data) - 2:
+            downrighttwo = downright(x,y,data,3)
+            if downrighttwo in hitstwo:
+                downlefttwo = downleft(x,y+2,data,3)
+                if downlefttwo in hitstwo:
+                    parttwo = parttwo + 1
 
-print(partone)           
+print(partone)
+print(parttwo)
