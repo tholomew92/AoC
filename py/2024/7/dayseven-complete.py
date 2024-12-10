c858f0c042b19682c0e0cd99aa7a3df2e8dfb415
unknown <sebastian.gabrielsson@gmail.com>
2024-12-10 19:18:56 +0100

    Finished day part 2

--- py/2024/7/dayseven-complete.py
+++ py/2024/7/dayseven-complete.py
@@ -1,6 +1,5 @@
 import os
 import time
-from itertools import product
 
 start_time = time.time()
 
@@ -9,7 +8,6 @@ testfilepath = os.path.join(here, "test.txt")
 inputfilepath = os.path.join(here, "input.txt")
 
 data = [line.rstrip() for line in open(inputfilepath)]
-debugvals = [641,109951,46913588,104773227,363819106,732348540]
 partone = 0
 parttwo = 0
 
@@ -23,36 +21,23 @@ def do_compute(vals, target, parttwo=False):
         
         for val in valsset:
             prod = val * v
-            if prod == target:
-                print(f"{val} * {v}")
-                return True
-            if prod < target:
+            if prod <= target:
                 tempset.append(prod)
             add = val + v
-            if add == target:
-                print(f"{val} + {v}")
-                return True
-            if add < target:
+            if add <= target:
                 tempset.append(add)
+            conc = 0
             if parttwo:
                 pwr = 10 ** len(str(v))
                 conc = val * pwr + v
-                #conc = int(str(val)+str(v))
-                if conc == target:
-                    print(f"{val} conc {v}")
-                    return True
-                if conc < target:
+                if conc <= target:
                     tempset.append(conc)
-            
-        #if target in debugvals and parttwo:
-            #print(tempset)
-        if not tempset:
-            break
+
         valsset = tempset
+    if target in valsset:
+        return True
     return False
         
-            
-
 for line in data:
     split = [l.strip() for l in line.split(':')]
     target = int(split[0])
@@ -67,16 +52,12 @@ for line in data:
     split = [l.strip() for l in line.split(':')]
     target = int(split[0])
     vals = [int(d) for d in split[1].split(' ')]
-    if target in debugvals:
-        print(target)
     restwo = (do_compute(vals, target, True))
     if restwo:
         parttwo += target
-    if target in debugvals:
-        print()
         
 
 parttwo_time = time.time() - partone_time - start_time
 
 print(f"Part One: {partone} It took {partone_time*1000:.3f} ms")
-print(f"Part Two: {parttwo} It took {parttwo_time*1000:.3f} ms")
+print(f"Part Two: {parttwo} It took {parttwo_time:.3f} sec")
