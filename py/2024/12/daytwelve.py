166dd53fed1ff8967ec6dae25201abad09156e60
unknown <sebastian.gabrielsson@gmail.com>
2024-12-13 00:23:10 +0100

    Finished day 12

--- py/2024/12/daytwelve.py
+++ py/2024/12/daytwelve.py
@@ -1,5 +1,6 @@
 import os
 import time
+import re
 
 start_time = time.time()
 
@@ -7,10 +8,10 @@ here = os.path.dirname(os.path.abspath(__file__))
 testfilepath = os.path.join(here, "test.txt")
 inputfilepath = os.path.join(here, "input.txt")
 
-data = [line.rstrip() for line in open(testfilepath)]
+data = [line.rstrip() for line in open(inputfilepath)]
 
 positions = []
-dirs = ((-1, 0),(0, -1),(1, 0),(0, 1))
+dirs = ((-1, 0),(0, 1),(1, 0),(0, -1))
 xrange = range(0, len(data))
 yrange = range(0, len(data[0]))
 partonescores = dict()
@@ -45,117 +46,90 @@ def find_edges(plot):
                 count += 1
     return count
 
-def going_up(pos):
-    dir = (-1, 0)
-    x,y = pos
-    if y - 1 in yrange and data[x][y] == data[x][y - 1]:
-        y -= 1
-        dir = (0, -1)
-    elif x - 1 in xrange and data[x][y] == data[x - 1][y]:
-        x -= 1
-    elif y + 1 in yrange and data[x][y] == data[x][y + 1]:
-        y += 1
-        dir = (0, 1)
-    else:
-        x += 1
-        dir = (1, 0)
-    return dir, (x,y)
-
-def going_right(pos):
-    dir = (0,1)
-    x,y = pos
-    if x - 1 in xrange and data[x][y] == data[x - 1][y]:
-        x -= 1
-        dir = (-1, 0)
-    elif y + 1 in yrange and data[x][y] == data[x][y + 1]:
-        y += 1
-    elif x + 1 in xrange and data[x][y] == data[x + 1][y]:
-        #print("Found down")
-        x += 1
-        dir = (1, 0)
-    else:
-        y -= 1
-        dir = (0, -1)
-    return dir, (x,y)
-
-def going_down(pos):
-    dir = (1, 0)
-    x,y = pos
-    if y + 1 in yrange and data[x][y] == data[x][y + 1]:
-        y += 1
-        dir = (0, 1)
-    elif x + 1 in xrange and data[x][y] == data[x + 1][y]:
-        x += 1
-    elif y - 1 in yrange and data[x][y] == data[x][y - 1]:
-        y -= 1
-        dir = (0, -1)
-    else:
-        x -= 1
-        dir = (-1, 0)
-    return dir, (x,y)
-
-def going_left(pos):
-    dir = (0, -1)
-    x,y = pos
-    if x + 1 in xrange and data[x][y] == data[x + 1][y]:
-        x += 1
-        dir = (1, 0)
-    elif y - 1 in yrange and data[x][y] == data[x][y - 1]:
-        y -= 1
-    elif x - 1 in xrange and data[x][y] == data[x - 1][y]:
-        x -= 1
-        dir = (-1, 0)
-    else:
-        y += 1
-        dir = (0, 1)
-    return dir, (x,y)
-
-def find_sides(pos):
-    count = 0
+def turn(pos, dirind):
     x, y = pos
-    fdor = dirs[0]
-    dir = dirs[0]
-    npos = (x,y)
-    while True:
-        #if pos == (0,6):
-            #time.sleep(1)
-            #print(f"{npos} {dir}")
-        if dir == (-1, 0):
-            #print("Going up")
-            ndir, npos = going_up(npos)
-            if ndir != dir:
-                if ndir == (1, 0):
+    if dirind == 0:
+        dirind = 4
+    dirind -= 1
+    ndirind = -1
+    for i in range(len(dirs)):
+        dx, dy = dirs[dirind]
+        if x + dx in xrange and y + dy in yrange:
+            if data[x][y] == data[x + dx][y + dy]:
+                pos = (x+dx,y+dy)
+                ndirind = dirind
+                break
+        dirind += 1
+        if dirind == 4:
+            dirind = 0
+
+    return pos, ndirind
+
+def find_sides(pos, plot):
+
+    count = 0
+    tx, ty = pos
+    target = data[tx][ty]
+    rowsstart = dict()
+    rowsend = dict()
+    colsstart = dict()
+    colssend = dict()
+
+    for x in xrange:
+        rowsstart[x] = []
+        rowsend[x] = []
+        row = ''.join(data[x])
+        pattern = re.escape(target) + r'+'
+        matches = re.finditer(pattern, row)
+        sections = [(match.start(), match.end() - 1) for match in matches]
+        for section in sections:
+            sy, ey = section
+            if (x,sy) in plot:
+                rowsstart[x].append(sy)
+            if (x, sy) in plot:
+                rowsend[x].append(ey)
+            
+    for x in xrange:
+        if x == 0:
+            count += len(rowsstart[0])
+            count += len(rowsend[0])
+        else:
+            for start in rowsstart[x]:
+                if start not in rowsstart[x - 1]:
                     count += 1
-                count += 1
-                dir = ndir
-        elif dir == (0, 1):
-            #print("Going right")
-            ndir, npos = going_right(npos)
-            if ndir != dir:
-                if ndir == (0, -1):
+            for end in rowsend[x]:
+                if end not in rowsend[x - 1]:
                     count += 1
-                count += 1
-                dir = ndir
-        elif dir == (1, 0):
-            #print("Going down")
-            ndir, npos = going_down(npos)
-            if ndir != dir:
-                if ndir == (-1, 0):
+        
+    cv = count
+
+    for y in yrange:
+        colsstart[y] = []
+        colssend[y] = []
+        columndata = [col[y] for col in data]
+        column = ''.join(columndata)
+        pattern = re.escape(target) + r'+'
+        matches = re.finditer(pattern, column)
+        sections = [(match.start(), match.end() - 1) for match in matches]
+        for section in sections:
+            sx, ex = section
+            if(sx,y) in plot:
+                colsstart[y].append(sx)
+            if(ex, y) in plot:
+                colssend[y].append(ex)
+
+    for y in yrange:
+        if y == 0:
+            count += len(colsstart[0])
+            count += len(colssend[0])
+        else:
+            for start in colsstart[y]:
+                if start not in colsstart[y - 1]:
                     count += 1
-                count += 1
-                dir = ndir
-        elif dir == (0, -1):
-            #print("Going left")
-            ndir, npos = going_left(npos)
-            if ndir != dir:
-                if ndir == (0, 1):
+            for end in colssend[y]:
+                if end not in colssend[y - 1]:
                     count += 1
-                count += 1
-                dir = ndir
-        if npos == pos and dir == fdor:
-            count += 1
-            break
-    print(f"{pos}: {count}")
+   
     return count
 
 visited = set()
@@ -171,15 +145,10 @@ for x in range(len(data)):
         visited.update(plot)
         plots.append(plot)
         partone += find_edges(plot) * len(plot)
-        if len(plot) == 1:
-            parttwo += 4
-            continue
-        parttwo += find_sides(pos) * len(plot)
-
-temp = find_sides((0,0))
+        parttwo += find_sides(pos, plot) * len(plot)
 
 end_time = time.time() - start_time
 
 print(partone)
 print(parttwo)
-print(f"Time taken: {end_time*1000:3f} ms")
+print(f"Time taken: {end_time:3f} s")
