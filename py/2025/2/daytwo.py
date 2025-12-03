1faea73189f051c6e2da0b73883136845b4b3edc
tholomew92 <sebastian.gabrielsson@gmail.com>
2025-12-03 14:36:44 +0100

    Started 2025 and finished day two

---
+++ py/2025/2/daytwo.py
@@ -0,0 +1,56 @@
+import os
+import math
+
+here = os.path.dirname(os.path.abspath(__file__))
+testfilepath = os.path.join(here, "test.txt")
+testfile = open(testfilepath, "r")
+inputfilepath = os.path.join(here, "input.txt")
+inputfile = open(inputfilepath, "r")
+
+file = [line.rstrip() for line in open(inputfilepath)]
+data = file[0]
+
+idranges = data.split(',')
+
+def find_factors(num):
+    factors = []
+    for i in range(1, int(math.sqrt(num)) + 1):
+        if i == num:
+            break
+        if num % i == 0:
+            factors.append(i)
+            if i != num // i:
+                factors.append(num // i)
+    return sorted(factors)
+
+p1 = 0
+p2 = 0
+loops = 0
+for idrange in idranges:
+    split = idrange.split('-')
+    start = int(split[0])
+    end = int(split[1]) + 1
+    for i in range(start, end):
+        parttwo = False
+        vstr = str(i)
+        slen = len(vstr)
+        if slen % 2 == 0:
+            v1 = vstr[0:slen//2]
+            v2 = vstr[slen//2:]
+            if v1 == v2:
+                parttwo = True
+                p1 += i
+        if not parttwo:
+            factors = find_factors(slen)
+            for factor in factors:
+                if(factor == slen):
+                    continue
+                sets = [vstr[j:j+factor] for j in range(0, slen, factor)]
+                if len(set(sets)) == 1:
+                    parttwo = True
+                    break
+        if parttwo:
+            p2 += i
+
+print(f"Part one: {p1}")
+print(f"Part two: {p2}")
