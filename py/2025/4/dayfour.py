9aa7792cd85c051e5b6f1c52bf6869b3e87ebe68
unknown <sebastian.gabrielsson@gmail.com>
2025-12-05 00:15:00 +0100

    Cleaned up day 4

--- py/2025/4/dayfour.py
+++ py/2025/4/dayfour.py
@@ -9,39 +9,25 @@ inputfilepath = os.path.join(here, "input.txt")
 
 def check_neighbours(rolls, hpos, wpos):
     hits = 0
-    minh = maxh = minw = maxw = -1
-    if hpos == 0:
-        minh = 0
-    else:
-        minh = hpos - 1
-    if hpos == height - 1:
-        maxh = hpos + 1
-    else:
-        maxh = hpos + 2
-    if wpos == 0:
-        minw = 0
-    else:
-        minw = wpos - 1
-    if wpos == width - 1:
-        maxw = wpos + 1
-    else:
-        maxw = wpos + 2
-    for h in range(minh , maxh):
-        for w in range(minw, maxw):
-            if h == hpos and w == wpos:
-                continue
-            else:
-                
-                if rolls[h][w] == '@':
-                    hits += 1
-    return hits
+    offsets = [(-1, -1), (-1, 0), (-1, 1), 
+               (0, -1),         (0, 1), 
+               (1, -1), (1, 0), (1, 1)]
+
+    for dh, dw in offsets:
+        h, w = hpos + dh, wpos + dw
+        if 0 <= h < height and 0 <= w < width:
+            if rolls[h][w] == '@':
+                hits += 1
+                if hits == 4:
+                    return False
+    return True
 
 def remove_rolls(rollmap):
     hitpos = []
     for h in range(height):
         for w in range(width):
             if rollmap[h][w] == '@':
-                if check_neighbours(rollmap, h, w) < 4:
+                if check_neighbours(rollmap, h, w):
                     hitpos.append((h,w))
     return hitpos
 
@@ -61,11 +47,11 @@ loops = 0
 
 while True:
     hits = remove_rolls(rolls)
+    if len(hits) == 0:
+        break
     if first:
         p1 = len(hits)
         first = False
-    if len(hits) == 0:
-        break
     p2 += len(hits)
     for h, w in hits:
         rolls[h][w] = '.'
